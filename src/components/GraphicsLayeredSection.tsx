5f18deb5b6f5efebc68f8dd85eb0cbebf745903e
bliss-armah <blissarmah8@gmail.com>
2025-11-04 08:49:32 +0000

    fix: correct asset filename for graphics in GraphicsLayeredSection

--- src/components/GraphicsLayeredSection.tsx
+++ src/components/GraphicsLayeredSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import DonationHeroSection from "./DonationHeroSection";
-import graphics from "../assets/Path.svg";
+import graphics from "../assets/PATH.svg";
 import giraffe from "../assets/girrafe.svg";
 
 const GraphicsLayeredSection: React.FC = () => {
